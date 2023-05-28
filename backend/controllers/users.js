d0cc058916611d8c79863a680857b105f74f9f7d
LogosLegend <75035186+LogosLegend@users.noreply.github.com>
2023-05-28 09:15:50 +0600

    Add files via upload

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -114,8 +114,6 @@ module.exports.login = (req, res, next) => {
 
       res.cookie("jwt", token, {
         httpOnly: true,
-        sameSite: "none",
-        secure: true,
         maxAge: 3600000 * 24 * 7
       }).send({ message: "Вход выполнен", token})
     })
