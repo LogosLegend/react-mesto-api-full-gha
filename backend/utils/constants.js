0fdd382f8e2a7bbad0e2850302faa812c9e72085
LogosLegend <75035186+LogosLegend@users.noreply.github.com>
2023-05-30 05:26:42 +0600

    Add files via upload

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,10 +1,17 @@
-const errorCodeMessage400 = 'Переданы некорректные данные',
-      errorCodeMessage401 = 'Неверный email или пароль',
-      errorCodeMessage403 = 'Недостаточно прав',
-      errorCodeUserMessage404 = 'Пользователь не найден',
-      errorCodeCardMessage404 = 'Карточка не найдена',
-      errorCodeUrlMessage404 = 'Страница не найдена',
-      errorCodeMessage409 = 'Пользователь уже зарегистрирован';
+const errorCodeMessage400 = 'Переданы некорректные данные';
+const errorCodeMessage401 = 'Неверный email или пароль';
+const errorCodeMessage403 = 'Недостаточно прав';
+const errorCodeUserMessage404 = 'Пользователь не найден';
+const errorCodeCardMessage404 = 'Карточка не найдена';
+const errorCodeUrlMessage404 = 'Страница не найдена';
+const errorCodeMessage409 = 'Пользователь уже зарегистрирован';
 
-
-module.exports = { errorCodeMessage400, errorCodeMessage401, errorCodeMessage403, errorCodeUserMessage404, errorCodeCardMessage404, errorCodeUrlMessage404, errorCodeMessage409 };
+module.exports = {
+  errorCodeMessage400,
+  errorCodeMessage401,
+  errorCodeMessage403,
+  errorCodeUserMessage404,
+  errorCodeCardMessage404,
+  errorCodeUrlMessage404,
+  errorCodeMessage409,
+};
