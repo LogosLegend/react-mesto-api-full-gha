c22fa84f9dd2618f79b26aafca7c32a3705316d3
LogosLegend <75035186+LogosLegend@users.noreply.github.com>
2023-05-30 05:25:52 +0600

    Add files via upload

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,20 +1,18 @@
 const jwt = require('jsonwebtoken');
+
 const { NODE_ENV, JWT_SECRET } = process.env;
 const Unauthorized = require('../errors/Unauthorized');
 
 module.exports = (req, res, next) => {
-
   const token = req.cookies.jwt;
   let payload;
 
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
-
   } catch (err) {
-
-    next(new Unauthorized('Авторизируйтесь'))
+    next(new Unauthorized('Авторизируйтесь'));
   }
 
   req.user = payload;
   next();
-}; 
+};
