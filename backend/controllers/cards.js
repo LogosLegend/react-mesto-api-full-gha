591a03cc4d2b459e11f7686ed1d039652adbfbde
LogosLegend <75035186+LogosLegend@users.noreply.github.com>
2023-05-30 05:25:02 +0600

    Add files via upload

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -1,96 +1,103 @@
-const Card = require('../models/card.js');
+const Card = require('../models/card');
 
-const BadRequest = require('../errors/BadRequest'),
-      Forbidden = require('../errors/Forbidden'),
-      NotFoundError = require('../errors/NotFoundError')
+const BadRequest = require('../errors/BadRequest');
+const Forbidden = require('../errors/Forbidden');
+const NotFoundError = require('../errors/NotFoundError');
 
 const {
 
   errorCodeMessage400,
   errorCodeMessage403,
-  errorCodeCardMessage404
-  
-} = require('../utils/constants.js');
+  errorCodeCardMessage404,
+
+} = require('../utils/constants');
 
 module.exports.getCards = (req, res, next) => {
-
   Card.find({})
-    .then(card => res.send(card))
+    .then((card) => res.send(card))
     .catch(next);
 };
 
 module.exports.createCard = (req, res, next) => {
-
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then(card => res.send(card))
+    .then((card) => res.send(card))
     .catch((err) => {
-      
-      err.name === 'ValidationError'
-      ? next(new BadRequest(errorCodeMessage400))
-      : next(err)
+      if (err.name === 'ValidationError') {
+        next(new BadRequest(errorCodeMessage400));
+      } else {
+        next(err);
+      }
     });
 };
 
 module.exports.deleteCard = (req, res, next) => {
-
   const { cardId } = req.params;
 
   Card.findById(cardId)
     .then((card) => {
-      console.log(req.user._id)
-      console.log(card)
-      card
-      ? req.user._id == card.owner._id
-        ? Card.findByIdAndRemove(cardId)
-            .then(() => res.send({ message: "Удаление выполнено" }))
-            .catch(next)
-        : next(new Forbidden(errorCodeMessage403))
-      : next(new NotFoundError(errorCodeCardMessage404))
+      if (card) {
+        if (req.user._id === card.owner._id.toString()) {
+          Card.findByIdAndRemove(cardId)
+            .then(() => res.send({ message: 'Удаление выполнено' }))
+            .catch(next);
+        } else {
+          next(new Forbidden(errorCodeMessage403));
+        }
+      } else {
+        next(new NotFoundError(errorCodeCardMessage404));
+      }
+    })
     .catch((err) => {
-      
-      err.name === 'CastError'
-      ? next(new BadRequest(errorCodeMessage400))
-      : next(err)
+      if (err.name === 'CastError') {
+        next(new BadRequest(errorCodeMessage400));
+      } else {
+        next(err);
+      }
     });
-  });
-}
+};
 
 module.exports.likeCard = (req, res, next) => {
-
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
-    { new: true })
+    { new: true },
+  )
     .then((card) => {
-      card
-      ? res.send(card)
-      : next(new NotFoundError(errorCodeCardMessage404))
+      if (card) {
+        res.send(card);
+      } else {
+        next(new NotFoundError(errorCodeCardMessage404));
+      }
+    })
     .catch((err) => {
-      
-      err.name === 'CastError'
-      ? next(new BadRequest(errorCodeMessage400))
-      : next(err)
+      if (err.name === 'CastError') {
+        next(new BadRequest(errorCodeMessage400));
+      } else {
+        next(err);
+      }
     });
-  });
-}
+};
 
 module.exports.deleteLikeCard = (req, res, next) => {
-
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
-    { new: true })
+    { new: true },
+  )
     .then((card) => {
-      card
-      ? res.send(card)
-      : next(new NotFoundError(errorCodeCardMessage404))
+      if (card) {
+        res.send(card);
+      } else {
+        next(new NotFoundError(errorCodeCardMessage404));
+      }
+    })
     .catch((err) => {
-      
-      err.name === 'CastError'
-      ? next(new BadRequest(errorCodeMessage400))
-      : next(err)
-  });
-  });
-}
+      if (err.name === 'CastError') {
+        next(new BadRequest(errorCodeMessage400));
+      } else {
+        next(err);
+      }
+    });
+};
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,10 +1,10 @@
-const bcrypt = require('bcryptjs'),
-      jwt = require('jsonwebtoken'),
-      User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
-const BadRequest = require('../errors/BadRequest'),
-      NotFoundError = require('../errors/NotFoundError'),
-      Conflict = require('../errors/Conflict');
+const BadRequest = require('../errors/BadRequest');
+const NotFoundError = require('../errors/NotFoundError');
+const Conflict = require('../errors/Conflict');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
@@ -12,117 +12,120 @@ const {
 
   errorCodeMessage400,
   errorCodeUserMessage404,
-  errorCodeMessage409
-  
-} = require('../utils/constants.js');
+  errorCodeMessage409,
+
+} = require('../utils/constants');
 
 module.exports.getUsers = (req, res, next) => {
-	
   User.find({})
-    .then(user => res.send(user))
+    .then((user) => res.send(user))
     .catch(next);
 };
 
 module.exports.getUser = (req, res, next) => {
-
   User.findById(req.user._id)
-    .then(user => res.send(user))
+    .then((user) => res.send(user))
     .catch(next);
 };
 
 module.exports.getUserId = (req, res, next) => {
-
   User.findById(req.params.userId)
     .then((user) => {
-      user
-      ? res.send(user)
-      : next(new NotFoundError(errorCodeUserMessage404))
+      if (user) {
+        res.send(user);
+      } else {
+        next(new NotFoundError(errorCodeUserMessage404));
+      }
+    })
     .catch((err) => {
-    
-    err.name === 'CastError'
-    ? next(new BadRequest(errorCodeMessage400))
-    : next(err)
+      if (err.name === 'CastError') {
+        next(new BadRequest(errorCodeMessage400));
+      } else {
+        next(err);
+      }
     });
-  });
 };
 
 module.exports.createUser = (req, res, next) => {
-
-  const { name, about, avatar, email, password } = req.body;
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
 
   bcrypt.hash(password, 10)
-    .then(hash => User.create({name, about, avatar, email, password: hash}))
-    .then(user => res.send(user))
+    .then((hash) => User.create({
+      name, about, avatar, email, password: hash,
+    }))
+    .then((user) => res.send(user))
     .catch((err) => {
-
-      err.name === 'ValidationError'
-      ? next(new BadRequest(errorCodeMessage400))
-      : err.code === 11000
-        ? next(new Conflict(errorCodeMessage409))
-        : next(err)
+      if (err.name === 'ValidationError') {
+        next(new BadRequest(errorCodeMessage400));
+      } else if (err.code === 11000) {
+        next(new Conflict(errorCodeMessage409));
+      } else {
+        next(err);
+      }
     });
 };
 
 module.exports.updateUserInfo = (req, res, next) => {
-
   const { name, about } = req.body;
 
   User.findByIdAndUpdate(
     req.user._id,
     { name, about },
-    { new: true, runValidators: true })
-  .then(user => res.send(user))
-  .catch((err) => {
-    
-    err.name === 'ValidationError'
-    ? next(new BadRequest(errorCodeMessage400))
-    : err.name === 'CastError'
-      ? next(new NotFoundError(errorCodeUserMessage404))
-      : next(err)
-  });
+    { new: true, runValidators: true },
+  )
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequest(errorCodeMessage400));
+      } else if (err.name === 'CastError') {
+        next(new NotFoundError(errorCodeUserMessage404));
+      } else {
+        next(err);
+      }
+    });
 };
 
 module.exports.updateUserAvatar = (req, res, next) => {
-
   const { avatar } = req.body;
 
   User.findByIdAndUpdate(
     req.user._id,
     { avatar },
-    { new: true, runValidators: true })
-  .then(user => res.send(user))
-  .catch((err) => {
-    
-    err.name === 'ValidationError'
-    ? next(new BadRequest(errorCodeMessage400))
-    : err.name === 'CastError'
-      ? next(new NotFoundError(errorCodeUserMessage404))
-      : next(err)
-  });
+    { new: true, runValidators: true },
+  )
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequest(errorCodeMessage400));
+      } else if (err.name === 'CastError') {
+        next(new NotFoundError(errorCodeUserMessage404));
+      } else {
+        next(err);
+      }
+    });
 };
 
 module.exports.login = (req, res, next) => {
-
   const { email, password } = req.body;
 
   User.findUserByCredentials(email, password)
-    .then(user => {
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
 
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', {expiresIn: '7d'});
-
-      res.cookie("jwt", token, {
+      res.cookie('jwt', token, {
         httpOnly: true,
-        maxAge: 3600000 * 24 * 7
-      }).send({ message: "Вход выполнен", token})
+        maxAge: 3600000 * 24 * 7,
+      }).send({ message: 'Вход выполнен', token });
     })
-    .catch(next)
+    .catch(next);
 };
 
 module.exports.exit = (req, res) => {
-
   res.clearCookie('jwt', {
-    sameSite: "none",
-    secure: true
+    sameSite: 'none',
+    secure: true,
   })
-  .send({ message: "Выход выполнен" })
-}
+    .send({ message: 'Выход выполнен' });
+};
